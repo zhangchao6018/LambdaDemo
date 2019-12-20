48793b83a7b4e41519fa67cacdfbae0efeefc413
zhangchao <18910146715@163.com>
2019-12-20 08:40:44 +0800

    ...

---
+++ src/main/java/com/demo/mycode/java8/TestStreamApi1.java
@@ -0,0 +1,9 @@
+package com.demo.mycode.java8;
+
+/**
+ * @Description:
+ * @Author: zhangchao
+ * @Date: 2019-12-20 07:47
+ **/
+public class TestStreamApi1 {
+}
---
+++ src/main/java/com/demo/mycode/java8/TestStreamApi2.java
@@ -0,0 +1,10 @@
+package com.demo.mycode.java8;
+
+/**
+ * @Description:
+ * @Author: zhangchao
+ * @Date: 2019-12-20 07:48
+ **/
+public class TestStreamApi2 {
+
+}
